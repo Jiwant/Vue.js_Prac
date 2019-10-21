2d7f210689a2eb38af6c162801efac55138f021d
Jiwant Singh <jiwantsingh01@gmail.com>
2019-10-21 12:33:03 -0400

    Initialization

---
+++ ImptCode/0_install.sh
@@ -0,0 +1,14 @@
+#Install Some Important Packages of VS Code
+
+_______________________________________________________________________
+#Install Vue CLI
+npm install -g @vue/cli
+
+#Specific Version(Specific to Pluralsight Course)
+npm install -g @vue/cli@3.0.0-rc.3
+_______________________________________________________________________
+#Create Application
+vue create build-a-bot
+_______________________________________________________________________
+#Run Vue.js Applications
+npm run server
